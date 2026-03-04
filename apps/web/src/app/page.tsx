6212b79e9f8c49d9af20cb26175eb6be78cc5038
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2026-03-03 21:42:13 -0300

    first commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+
+    return (
+        <main className="flex flex-col items-center justify-center py-10 px-96 h-full w-full gap-5 bg-[#11100f]">
+            <h1>Ola, Mundo!</h1>
+        </main>
+    );
+}
